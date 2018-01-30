490ac00e0635e84cf9610cebcc86670fb6f3059f
asad149 <asad.arshad.94849@gmail.com>
2018-01-30 20:36:41 +0500

    Second Commit

---
+++ Vowels_Counting.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication11
+{
+    class Vowels
+    {
+
+
+        public static void Counting_Vowels()
+        {
+            Console.WriteLine("\t\t\t************ Counting of Vowels ************\n\n ");
+            int count = 0;
+            char[] a = new char[10];
+            Console.Write("Enter Any Name : ");
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = Console.ReadKey().KeyChar;
+
+                if (a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u' || a[i] == 'A' || a[i] == 'E' || a[i] == 'I' || a[i] == 'O' || a[i] == 'U')
+                {
+                    count++;
+                }
+                
+            }
+            Console.WriteLine("\n\nVowels Present are {0}", count);
+        }
+
+       
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vowels.Counting_Vowels();
+
+            Console.ReadLine();
+        }
+    }
+}
